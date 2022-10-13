ecddd9392745c7d71bcb1df5dff63adf4c3cacea
Akhilkarnati002 <akhilkarnati000@gmail.com>
2022-10-13 22:56:39 +0530

    added the firstname in login

--- login/forms.py
+++ login/forms.py
@@ -4,5 +4,5 @@ from .models import MyModel
 class MyForm(forms.ModelForm):
   class Meta:
     model = MyModel
-    fields = ["username", "email","password","cpassword"]
+    fields = ["username", "email","password","cpassword","firstname"]
     labels = {'username': "Name", "email": "E-mail","password":"Password","cpassword":"Confirm Password"}
